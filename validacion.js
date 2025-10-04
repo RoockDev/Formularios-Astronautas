14146753594a9623af2f1eeb772da7a855ef8482
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-04 11:30:27 +0200

    feat: Metodos de validacion
    Se implementan los metodos de validacion de nombre,correo y password

---
+++ validacion.js
@@ -0,0 +1,93 @@
+const validateFullName = () => {
+  let fullNameInput = document.getElementById("fullName");
+  let errorFullName = document.getElementById("error-fullName");
+
+  fullNameInput.addEventListener("input", () => {
+    const value = fullNameInput.value.trim(); //quitamos los espacios del inicio y del final
+
+    if (value.length === 0) {
+      fullNameInput.style.borderColor = "red";
+      fullNameInput.style.backgroundColor = "#ffcccc";
+      fullNameInput.style.color = "black";
+      errorFullName.textContent = "El campo de nombre no puede estar vacio";
+    } else if (value.length < 3) {
+      fullNameInput.style.borderColor = "red";
+      fullNameInput.style.backgroundColor = "#ffcccc";
+      fullNameInput.style.color = "black";
+      errorFullName.textContent =
+        "Este campo no puede tener menos de 3 caracteres";
+    } else {
+      fullNameInput.style.borderColor = "green";
+      fullNameInput.style.backgroundColor = "#333";
+      fullNameInput.style.color = "white";
+
+      errorFullName.textContent = "";
+    }
+  });
+};
+
+const isValidEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+const validateEmail = () =>{
+    let email = document.getElementById('email');
+    let errorEmail = document.getElementById('error-email');
+
+    email.addEventListener('input', () => {
+        const value = email.value.trim(); 
+
+        if (value.length === 0) {
+            email.style.borderColor = 'red';
+            email.style.backgroundColor = "#ffcccc";
+            email.style.color = 'black';
+            errorEmail.textContent = 'El campo email no puede estar vacio';
+        }else if(!isValidEmail(value)){
+          email.style.borderColor = 'red';
+          email.style.backgroundColor = "#ffcccc";
+          email.style.color = 'black';
+          errorEmail.textContent = 'El Correro no tiene un formato valido';
+        }else{
+          email.style.borderColor = 'green';
+          email.style.backgroundColor = "#333";
+          email.style.color = 'white';
+          errorEmail.textContent = "";
+        }
+    });
+}
+
+const isValidPassword = (password) => {
+  const regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*]).{8,}$/;
+  return regex.test(password);
+}
+
+const validatePassword = () =>{
+  let  password = document.getElementById('password');
+  let errorPassword = document.getElementById('error-password');
+
+  password.addEventListener('input', () =>{
+    const value = password.value.trim();
+    if (value.length === 0){
+      password.style.borderColor = 'red';
+      password.style.backgroundColor = '#ffcccc';
+      password.style.color = 'black';
+      errorPassword.textContent = 'El campo Password no puede estar vacio';     
+    }else if (!isValidPassword(value)) {
+      password.style.borderColor = 'red';
+      password.style.backgroundColor = '#ffcccc';
+      password.style.color = 'black';
+      errorPassword.textContent = 'La contraseña debe contener al menos una mayúscula, un número,un símbolo especial y minimos 8 caracteres';    
+    } else {
+      password.style.borderColor = 'green';
+      password.style.backgroundColor = '#333';
+      errorPassword.textContent = '';
+    }
+  });
+}
+
+
+
+validateFullName();
+validateEmail();
+validatePassword();
